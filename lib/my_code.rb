eb30aec847ef65d90d18d33a5564b3fed7203012
Michael Gerland <mr.gerland@gmail.com>
2019-11-04 19:18:50 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,14 +1,1 @@
 # Your Code Here
-def map(array)
-  new = []
-  index = 0
-  while index < array.length
-    new << yield(array[index])
-    index += 1
-  end
-  new
-end
-
-def reduce(array, sv=nil)
-
-end
